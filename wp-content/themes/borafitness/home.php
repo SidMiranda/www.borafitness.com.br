c8aca9c90cbcab07dc50928526039b0afd1bf2a6
Sidney Miranda <41812064+SidMiranda@users.noreply.github.com>
2023-01-09 22:59:01 -0300

    add home page

--- wp-content/themes/borafitness/home.php
+++ wp-content/themes/borafitness/home.php
@@ -1,21 +1,27 @@
 <?php
-
 /* Template Name: Home */
-
 get_header(); 
-
-
+@include_once('parts/new-header.php');
 ?>
 
 <div class="container">
-
-	<h3>#Esportes, #Saúde, #BemEstar</h3>
-	<h1>#borafitness</h1>
-
-	<img src="<?php echo get_template_directory_uri(); ?>/assets/comming.png" alt="Comming Soom">
-
-	<h2>Juntos podemos mudar o mundo</h2>
-
+	<img src="<?php echo get_template_directory_uri(); ?>/assets/banner.png" alt="Main Banner">
+	<div id="slogan"><p>SAÚDE, ENERGIA E QUALIDADE DE VIDA</p></div>
+	<h3>Acesso nosso conteúdo exclusivo e fique por dentro das novidades</h4><br>
 </div>
 
-<?php get_footer(); ?>
+<?php 
+@include_once('parts/resumo-posts.php');
+
+?>
+<div class="container" style="margin-top:0px!important">
+	
+	<img id="banner2" src="<?php echo get_template_directory_uri(); ?>/assets/banner4.png" alt="Main Banner">
+</div>
+
+<div class="container">
+	<div id="rodape"><p>Todos os direitos reservados | @borafitness</p></div>
+</div>
+<?php
+get_footer(); 
+?>
--- wp-content/themes/borafitness/parts/new-header.php
+++ wp-content/themes/borafitness/parts/new-header.php
@@ -9,19 +9,19 @@ $youtube = get_field('link_youtube', 'options');
 
 <header id="headerBlock" style="background-color:<?= $headerColor; ?>">
 	<div class="container">
-			<nav class="busca" id="buscaHome">
-					<ul class="menu" id="menuHome">
-							<li><a href="<?php echo get_home_url(); ?>" style="color:<?= $menuColor.'!important'; ?>">sid</a></li>
-							<li><a href="<?php echo get_home_url().'/blog'; ?>" style="color:<?= $menuColor.'!important'; ?>">Blog</a></li>
-					</ul>
-			</nav>
-			<div class="img-logo">            
-					<a href="#"> 
-							<img src="<?= $logo['url'] ?>" alt="Blog">
-					</a>
-			</div>
-			<nav class="busca" id="buscaRedes">
-        <ul class="menu" id="menuRedes">
+		<nav id="menu">
+			<ul>
+				<li><a href="<?php echo get_home_url(); ?>" style="color:<?= $menuColor.'!important'; ?>">HOME</a></li>
+				<li><a href="<?php echo get_home_url().'/blog'; ?>" style="color:<?= $menuColor.'!important'; ?>">BLOG</a></li>
+			</ul>
+		</nav>
+		<div id="logo">            
+				<a href="#"> 
+						<img src="<?= $logo['url'] ?>" alt="Blog">
+				</a>
+		</div>
+		<nav id="redes">
+        <ul>
 					<a href="<?= $instagram ?>" target="_blank">
 						<img src="<?php echo get_template_directory_uri() . '/img/insta-white.svg'; ?>" data-svg="<?php echo get_template_directory_uri() . '/img/insta-white.svg'; ?>" alt="Rede Social">
 					</a>
@@ -49,10 +49,8 @@ if (headerBlock) {
 			scrollTop = document.documentElement.scrollTop;
 			if (scrollTop > 50) {
 				headerBlock.classList.add("on");
-				imgLogo.classList.add("onLogo");
 			} else {
 				headerBlock.classList.remove("on");
-				imgLogo.classList.remove("onLogo");
 			}
 		}
 	})();
